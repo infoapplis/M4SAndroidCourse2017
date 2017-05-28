290cabde618cab291106bd8e6385332c357e5d23
El Hadji Lamine Biaye <biaye89@gmail.com>
2017-05-28 20:54:09 +0000

    HelloWord | MinSDK19 | TargetSDK24

---
+++ HelloWord/HelloWorld/app/src/main/java/org/mobilesenegal/helloworld/HelloWordActivity.java
@@ -0,0 +1,13 @@
+package org.mobilesenegal.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_word);
+    }
+}
